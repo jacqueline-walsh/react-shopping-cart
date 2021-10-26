0719917bd13cc0690841582f76b343b52ff3ec7a
jacqueline-walsh <jacqueline.walsh34@gmail.com>
2021-10-26 09:02:53 +0100

    reformat divs

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,28 +1,29 @@
-import React from 'react';
+import React from "react";
 // Products
-const Products = ({products}) => {
-    console.log("Products ", products)
-    return (
-        <div>
-            <ul className="products">
-                {Object.values(products).map((product) => (
-                    <li key={product._id}>
-                        {console.log("Product Title ", product.title)}
-                        <div className="product">
-                            <a href={"#" + product._id}>
-                                <img src={product.image} alt={product.title} />
-                                <p>{product.title}</p>
-                            </a>
-                        </div>
-                        <div className="product-price">
-                            <div>{product.price}</div>
-                            <button className="button primary">Add To Cart</button>
-                            </div>
-                    </li>
-                ))}
-            </ul>
-        </div>
-    )
-}
+const Products = ({ products }) => {
+  console.log("Products ", products);
+  return (
+    <div>
+      <ul className="products">
+        {Object.values(products).map((product) => (
+          <li key={product._id}>
+            <div className="product">
+              <a
+                href={"#" + product._id}
+              >
+                <img src={product.image} alt={product.title}></img>
+                <p>{product.title}</p>
+              </a>
+              <div className="product-price">
+                <div></div>
+                <button className="button primary">Add To Cart</button>
+              </div>
+            </div>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
 
-export default Products
+export default Products;

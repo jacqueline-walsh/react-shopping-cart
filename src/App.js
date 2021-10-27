3cdb17147f293465107ae124ecdde80106850fb1
jacqueline-walsh <jacqueline.walsh34@gmail.com>
2021-10-27 17:56:27 +0100

    add redux products

--- src/App.js
+++ src/App.js
@@ -3,44 +3,48 @@ import data from "./data.json";
 import Products from "./components/Products";
 import Filter from "./components/Filter";
 import Cart from "./components/Cart";
+import store from "./store";
+import { Provider } from "react-redux";
 
 class App extends React.Component {
   constructor() {
     super();
     this.state = {
-      cartItems: localStorage.getItem("cartItems") ? JSON.parse(localStorage.getItem("cartItems")) : [],
+      cartItems: localStorage.getItem("cartItems")
+        ? JSON.parse(localStorage.getItem("cartItems"))
+        : [],
       products: data.products,
       size: "",
       sort: "",
     };
   }
   render() {
-
     const removeFromCart = (product) => {
       const cartItems = this.state.cartItems.slice();
       this.setState({
-        cartItems: cartItems.filter(x => x._id !== product._id)
+        cartItems: cartItems.filter((x) => x._id !== product._id),
       });
       localStorage.setItem(
-        "cartItems", 
-        JSON.stringify(cartItems.filter(x => x._id !== product._id)));
-    }
+        "cartItems",
+        JSON.stringify(cartItems.filter((x) => x._id !== product._id))
+      );
+    };
 
     const addToCart = (product) => {
       const cartItems = this.state.cartItems.slice();
       let alreadyInCart = false;
-      cartItems.forEach(item => {
+      cartItems.forEach((item) => {
         if (item._id === product._id) {
           item.count++;
           alreadyInCart = true;
         }
-      })
+      });
       if (!alreadyInCart) {
-        cartItems.push({...product, count: 1})
+        cartItems.push({ ...product, count: 1 });
       }
-      this.setState({cartItems});
+      this.setState({ cartItems });
       localStorage.setItem("cartItems", JSON.stringify(cartItems));
-    }
+    };
 
     const sortProducts = (e) => {
       const sort = e.target.value;
@@ -81,34 +85,47 @@ class App extends React.Component {
 
     const createOrder = (order) => {
       alert("need to save order for " + order.name);
-    }
+    };
 
     return (
-      <div className="grid-container">
-        <header>
-          <a href="/">React Shopping Cart</a>
-        </header>
-        <main>
-          <div className="content">
-            <div className="main">
-              <Filter
-                count={this.state.products.length}
-                size={this.state.size}
-                sort={this.state.sort}
-                filterProducts={filterProducts}
-                sortProducts={sortProducts}
-              />
-               {this.state.products.length === 0 ? <div className="product-message"><h3>There are currently no items available in this size</h3></div> : 
-                  <Products products={this.state.products} addToCart={addToCart}/>
-              }
+      <Provider store={store}>
+        <div className="grid-container">
+          <header>
+            <a href="/">React Shopping Cart</a>
+          </header>
+          <main>
+            <div className="content">
+              <div className="main">
+                <Filter
+                  count={this.state.products.length}
+                  size={this.state.size}
+                  sort={this.state.sort}
+                  filterProducts={filterProducts}
+                  sortProducts={sortProducts}
+                />
+                {this.state.products.length === 0 ? (
+                  <div className="product-message">
+                    <h3>There are currently no items available in this size</h3>
+                  </div>
+                ) : (
+                  <Products
+                    products={this.state.products}
+                    addToCart={addToCart}
+                  />
+                )}
+              </div>
+              <div className="sidebar">
+                <Cart
+                  cartItems={this.state.cartItems}
+                  removeFromCart={removeFromCart}
+                  createOrder={createOrder}
+                />
+              </div>
             </div>
-            <div className="sidebar">
-              <Cart cartItems={this.state.cartItems} removeFromCart={removeFromCart} createOrder={createOrder}/>
-            </div>
-          </div>
-        </main>
-        <footer>All rights reserved</footer>
-      </div>
+          </main>
+          <footer>All rights reserved</footer>
+        </div>
+      </Provider>
     );
   }
 }
--- src/components/Products.js
+++ src/components/Products.js
@@ -1,31 +1,55 @@
 import React from "react";
-import formatCurrency from '../util';
+import formatCurrency from "../util";
+import { connect } from "react-redux";
+import fetchProducts from "../actions/productAction";
 
-// Products
-const Products = ({ products, addToCart }) => {
-  console.log("Products ", products);
-  return (
-    <div>
-      <ul className="products">
-        {Object.values(products).map((product) => (
-          <li key={product._id}>
-            <div className="product">
-              <a
-                href={"#" + product._id}
-              >
-                <img src={product.image} alt={product.title}></img>
-                <p>{product.title}</p>
-              </a>
-              <div className="product-price">
-                <div>{formatCurrency(product.price)}</div>
-                <button onClick={() => addToCart(product)} className="button primary">Add To Cart</button>
-              </div>
-            </div>
-          </li>
-        ))}
-      </ul>
-    </div>
-  );
-};
+class Products extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      product: null,
+    };
+  }
 
-export default Products;
+  componentDidMount() {
+    this.props.fetchProducts();
+  }
+
+  render() {
+    const { product } = this.state;
+    console.log(this.props.products);
+
+    return (
+      <div>
+        {!this.props.products ? (
+          <div>Loading...</div>
+        ) : (
+          <ul className="products">
+            {this.props.products.map((prod) => (
+              <li key={prod._id}>
+                <div className="product">
+                  <a href={"#" + prod._id}>
+                    <img src={prod.image} alt={prod.title}></img>
+                    <p>{prod.title}</p>
+                  </a>
+                  <div className="product-price">
+                    <div>{formatCurrency(prod.price)}</div>
+                    <button
+                      onClick={() => this.props.addToCart(product)}
+                      className="button primary"
+                    >
+                      Add To Cart
+                    </button>
+                  </div>
+                </div>
+              </li>
+            ))}
+          </ul>
+        )}
+      </div>
+    );
+  }
+}
+export default connect((state) => ({ products: state.products.items }), {
+  fetchProducts,
+})(Products);
